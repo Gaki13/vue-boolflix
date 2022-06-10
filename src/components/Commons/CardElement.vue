d58c43c2c7fc69bfd5e0e955d42446ae93f275fd
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-10 17:03:09 +0200

    stampa delle flags senza gestione delle eccezioni

--- src/components/Commons/CardElement.vue
+++ src/components/Commons/CardElement.vue
@@ -2,20 +2,30 @@
   <div class="card">
       <h2>{{Film.title}}</h2>
       <h3>{{Film.original_title}}</h3>
-      <p>{{Film.original_language}}</p>
-      <p>{{Film.vote_average}}</p>
+      <p >Lingua originale: <lang-flag :iso=" SelectFlag(Film.original_language)"/></p>
+      <p>Voto: {{Film.vote_average}}</p>
   </div>
 </template>
 
 <script>
+import LangFlag from 'vue-lang-code-flags';
+
 export default {
     name: 'CardElement',
     props: {
         Film: Object,
-    }
+    },
+    components: {
+        LangFlag,
+
+    },
+    methods:{  
+        SelectFlag(language){
+            return language == "vi" ? "it , es , en , fr, de" : language ;
+        }
+    },
 }
 </script>
-
 <style lang="scss" scoped>
 
 </style>
